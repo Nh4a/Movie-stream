6d2081f3d150512327685b25447f6db9080f2b00
Nh4a <you@example.com>
2026-02-10 14:06:22 +0700

    fix nav 2

--- src/Components/BottomBar.jsx
+++ src/Components/BottomBar.jsx
@@ -3,46 +3,22 @@ import { Search } from "lucide-react";
 import { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 const BottomBar = () => {
- const location = useLocation()
+  const location = useLocation();
   return (
     <div className=" px-4 w-full  items-center bg-black fixed bottom-0 md:hidden z-100">
       <ul className="flex items-center justify-between font-bold text-gray-300 h-10 pr-10 sm:w-100 mx-auto">
-        <li className={location.pathname==="/"? "text-red-500":''}>
-          <Link
-            to={"/"}
-            onClick={() => {
-              setActiveHome(true);
-              setActiveMovie(false);
-              setActiveOffer(false);
-            }}
-            className="hover:text-white "
-          >
+        <li className={location.pathname === "/" ? "text-red-500" : ""}>
+          <Link to={"/"} className="hover:text-white ">
             HOME
           </Link>
         </li>
-        <li className={location.pathname==="/movie"? "text-red-500":''}>
-          <Link
-            to={"/movie"}
-            onClick={() => {
-              setActiveHome(false);
-              setActiveMovie(true);
-              setActiveOffer(false);
-            }}
-            className="hover:text-white"
-          >
+        <li className={location.pathname === "/movie" ? "text-red-500" : ""}>
+          <Link to={"/movie"} className="hover:text-white">
             MOVIE
           </Link>
         </li>
-        <li className={location.pathname==="/movie"? "text-red-500":''}>
-          <Link
-          to={'/movie'}
-            onClick={() => {
-              setActiveHome(false);
-              setActiveMovie(false);
-              setActiveOffer(true);
-            }}
-            className="hover:text-white "
-          >
+        <li className={location.pathname === "/movie" ? "text-red-500" : ""}>
+          <Link to={"/movie"} className="hover:text-white ">
             OFFERS
           </Link>
         </li>
@@ -53,7 +29,6 @@ const BottomBar = () => {
         </li>
       </ul>
     </div>
-
   );
 };
 export default BottomBar;
