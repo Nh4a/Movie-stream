a056c9b0ac010cbeb0401687e77e00af41f9c6ee
Nh4a <you@example.com>
2026-02-10 13:53:32 +0700

    fix nav

--- src/Components/BottomBar.jsx
+++ src/Components/BottomBar.jsx
@@ -5,7 +5,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 const BottomBar = () => {
  const location = useLocation()
   return (
-    <div className=" px-4 w-full  items-center bg-black fixed bottom-0 md:hidden">
+    <div className=" px-4 w-full  items-center bg-black fixed bottom-0 md:hidden z-100">
       <ul className="flex items-center justify-between font-bold text-gray-300 h-10 pr-10 sm:w-100 mx-auto">
         <li className={location.pathname==="/"? "text-red-500":''}>
           <Link
